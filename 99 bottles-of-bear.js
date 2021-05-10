23d7f0c2c7a62b4e5f273385c57ab80e556fb589
tepsurkaev <unknownsayid@mail.ru>
2021-05-10 16:29:53 +0300

    завершенная

--- 99 bottles-of-bear.js
+++ 99 bottles-of-bear.js
@@ -1,6 +1,26 @@
 function generateSongText(text) {
     for(let i = text; i >= 1 ; i--) {
-        console.log(`${i} бутылок пива на стене ${i} бутылок пива! Возьми одну, пусти по кругу ${i - 1} бутылок пива на стене!`);
+        let w1 = bottles(i);
+        let w2 = bottles(i - 1);
+        if (i -1 === 0) {
+            console.log(`${i} ${w1} пива на стене, ${i} ${w1} пива! Возьми одну, пусти по кругу, нет бутылок пива на стене!`);
+        } else {
+            console.log(`${i} ${w1} пива на стене, ${i} ${w1} пива! Возьми одну, пусти по кругу, ${i - 1} ${w2} бутылок пива на стене!`);
+        }
     }
 }
 generateSongText(99);
+
+function  bottles(num) {
+    if (num % 10 === 1 && num % num !== 11) {
+        return 'бутылка';
+    }
+    else if (num % 10 >= 2 && num % 10 < 5) {
+        return 'бутылки';
+    }
+    else if (num >= 5 && num <= 99 || num == 0) {
+        return 'бутылок';
+    }
+}
+
+
